d3a62a9d8db235df02ad3262272169f510cae8f3
MonkeyPC <llmdktopone@gmail.com>
2025-06-12 13:15:12 +0300

    basetest added for launch and close driver, first test added

---
+++ src/test/java/com/automationteststore/BaseTest.java
@@ -0,0 +1,29 @@
+package com.automationteststore;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get(Config.get("url"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
