9ad00517927701ee6fe833bb97eaab09d337ce9a
MonkeyPC <llmdktopone@gmail.com>
2025-06-12 11:27:58 +0300

    repo init

---
+++ src/main/java/com/automationteststore/Config.java
@@ -0,0 +1,20 @@
+package com.automationteststore;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = Config.class.getClassLoader().getResourceAsStream("config.properties")) {
+            props.load(input);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return props.getProperty(key);
+    }
+}
